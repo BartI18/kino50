34968a859e3c266a60be6334cc7674cda1fe9df3
Boris <borasa123@gmail.com>
2019-03-26 00:01:43 +0200

    Version 4

--- js/main.js
+++ js/main.js
@@ -1,28 +1,19 @@
 fetch("https://jsonplaceholder.typicode.com/photos").then(function (res) {
     console.log(res.status);
-    if(res.status===200)
-     return res.json();
+    if (res.status === 200)
+        return res.json();
 }).then(function (res) {
-    var navBar = document.getElementsByClassName("ListWithFilms")[0];
-    images = res.slice(0,50);
-    images.forEach(function (obj,i) {
-        let div = document.createElement("div");
-        let image = document.createElement("img");
-        let aHref = document.createElement("a");
-        div.className="FilmItem";
-        image.className= "FilmItem-image";
-        aHref.className="FilmItem-linkFilm";
-        image.src=obj.thumbnailUrl;
-        image.addEventListener("click",function () {
-           image.src=obj.url;
-        });
-        aHref.innerText = obj.title;
-        div.appendChild(image);
-        div.appendChild(aHref);
-
-        let lItem = document.createElement("li");
-        lItem.appendChild(div);
-        navBar.appendChild(lItem);
-
+    var navBar = document.querySelector(".ListWithFilms");
+    const amountElements = 50;
+    images = res.slice(0, amountElements);
+    images.forEach(function (obj, i) {
+        let filmItem = document.querySelector(".FilmItem");
+        let image = filmItem.content.querySelector(".FilmItem-image");
+        filmItem.content.querySelector(".FilmItem-linkFilm").innerText = obj.title;
+        image.src = obj.thumbnailUrl;
+        filmItem.onclick = function () {
+            image.src = obj.url;
+        };
+        navBar.appendChild(filmItem.content.cloneNode(true));
     })
 });
